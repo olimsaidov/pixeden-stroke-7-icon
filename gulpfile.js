5f4011cca6eff272e836143e2ab7286649b01f5f
Philipp Alferov <philipp.alferov@gmail.com>
2015-05-24 21:13:52 -0400

    Initial gulp & sass setup

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var minifycss = require('gulp-minify-css');
+var rename = require('gulp-rename');
+var plumber = require('gulp-plumber');
+var path = {
+  assets: 'pe-icon-7-stroke',
+  get sass() {
+    return this.assets + '/scss'
+  },
+  get css() {
+    return this.assets + '/css'
+  },
+  get dist() {
+    return this.assets + '/dist'
+  }
+};
+var fontName = 'pe-icon-7-stroke';
+
+gulp.task('sass', function() {
+  return gulp.src(path.sass + '/' + fontName + '.scss')
+  .pipe(plumber())
+  .pipe(sass())
+  .pipe(gulp.dest(path.dist));
+});
+
+gulp.task('watch', function() {
+  gulp.watch(
+    [path.sass + '/*.scss'],
+    ['sass']
+  );
+});
+
+gulp.task('default', function() {
+
+});
